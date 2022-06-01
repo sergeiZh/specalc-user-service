f44ba025856a9767582b807ae595d06de21ab837
Sergei Zhabrikov <edu.diff@gmail.com>
2022-06-01 18:09:27 +0200

    Initial commit. Just an empty project

---
+++ src/test/java/com/zh/sergei/specalc/spending/calculator/user/service/SpendingCalculatorUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.sergei.specalc.spending.calculator.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingCalculatorUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
